9194edd38ef814ae3d64a755b2ac95a54b162f6a
Konstibob <konsti.bobenko@gmail.com>
2020-09-02 22:42:16 +0200

    transitioned to CSS Grid + RBW CLRS + CLEAR Function

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 const squarespace = document.getElementById("squarespace")
-let color ="red"
+const clear_btn = document.getElementById("clearbutton")
+const Rainbow_btn = document.getElementById("rainbowbutton")
+let RBW_CLRS = false
+let defaultcolor = "red"
 
 
-for(i=0;i<16;i++){
-    const br = document.createElement("br");
-    squarespace.appendChild(br);
+for(i=0; i<16; i++){
+    // const br = document.createElement("br");
+    // squarespace.appendChild(br);
 
-    for(j=0;j<16;j++){
+    for(j=0; j<16; j++){
         const element = document.createElement("div");
         const classAtr = document.createAttribute("class")
-        classAtr.value ="square"
+        classAtr.value = "square"
         element.setAttributeNode(classAtr);
         squarespace.appendChild(element);
         element.addEventListener("mouseover", colorchange);
     }
 }
 
+
+clear_btn.addEventListener("click", clearBoard);
+
+Rainbow_btn.addEventListener("click", switchRainbow)
+
 function colorchange(){
 
-    this.style.backgroundColor = color;
-
+    if(RBW_CLRS){
+        this.style.backgroundColor = getRandomColor();
+    }
+    else{
+        this.style.backgroundColor = defaultcolor;
+    }
 }
+
+function switchRainbow(){
+    RBW_CLRS = !RBW_CLRS
+}
+
+function clearBoard(){
+
+    var c = squarespace.children;
+    
+    for(i=0; i<c.length; i++){
+        c[i].style.backgroundColor = "grey";
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
